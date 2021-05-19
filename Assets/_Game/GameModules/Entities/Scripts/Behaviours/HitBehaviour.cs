2f42d89714dfeadb4c193bf2cb3a6e37572ab5cd
Samuel Souza Cabral <samuel.s.cabral@hotmail.com>
2021-05-19 20:56:58 -0300

    Finalizando boss

---
+++ Assets/_Game/GameModules/Entities/Scripts/Behaviours/HitBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace _Game.GameModules.Entities.Scripts.Behaviours
+{
+    public class HitBehaviour : StateMachineBehaviour
+    {
+        Entity _entity;
+        [SerializeField] int hitImpact;
+
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            if (_entity == null && !animator.transform.TryGetComponent(out _entity)) return;
+            if (hitImpact > 1)
+            {
+                _entity.movement.Stop();
+            }
+        }
+
+        // OnStateExit is called before OnStateExit is called on any state inside this state machine
+        public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            animator.SetBool(AnimatorParams.ReceivingHit, false);
+        }
+    }
+}
