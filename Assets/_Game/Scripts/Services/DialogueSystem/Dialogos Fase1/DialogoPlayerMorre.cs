b908ab8b31c4a6e451a0094243cc8776c178df9d
Guilherme Macedo <gui.oms31@gmail.com>
2021-06-02 00:55:04 -0300

    Cutscene final e algumas correções de bug

---
+++ Assets/_Game/Scripts/Services/DialogueSystem/Dialogos Fase1/DialogoPlayerMorre.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using _Game.GameModules.Entities.Scripts;
+using Fluent;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DialogoPlayerMorre : FluentScript
+{
+    [SerializeField] UnityEvent onStart;
+    [SerializeField] UnityEvent onFinish;
+
+    public override FluentNode Create()
+    {
+        return
+            Yell(4.5f, "General:\nHAR HAR HAR HAR. E eu achando que iria me divertir um pouco mais!");
+    }
+
+    public override void OnFinish()
+    {
+        onFinish.Invoke();
+    }
+
+    public override void OnStart()
+    {
+        onStart.Invoke();
+    }
+}
