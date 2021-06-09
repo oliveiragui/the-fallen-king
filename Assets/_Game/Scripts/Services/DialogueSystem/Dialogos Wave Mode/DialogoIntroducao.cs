7e9be6f49891f266a3f6e755bad02038cb7c04d8
Samuel Souza Cabral <samuel.s.cabral@hotmail.com>
2021-06-09 20:06:58 -0300

    Modo Onda aprimorado

---
+++ Assets/_Game/Scripts/Services/DialogueSystem/Dialogos Wave Mode/DialogoIntroducao.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Fluent;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace _Game.Scripts.Services.DialogueSystem.Dialogos_Wave_Mode
+{
+    public class DialogoIntroducao : FluentScript
+    {
+        public GameObject timeline;
+        public UnityEvent onFinish;
+
+        public override FluentNode Create()
+        {
+            return
+                Show() *
+                Write("Sieg:\nEles destruiram a vida do meu povo, nos enterraram nas cinzas do que tinhamos...").WaitForButton() *
+                Write("Sieg:\nE Preciso parar-los antes que destruam outros povos").WaitForButton() *
+                Do(() => onFinish.Invoke()) *
+                Hide();
+        }
+    }
+}
---
+++ Assets/_Game/Scripts/Services/DialogueSystem/Dialogos Wave Mode/IniciaFogareu.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using _Game.GameModules.Entities.Scripts;
+using Fluent;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class IniciaFogareu : FluentScript
+{
+    [SerializeField] Entity character;
+    [SerializeField] UnityEvent onStart;
+    [SerializeField] UnityEvent onFinish;
+    [SerializeField] UnityEvent changeScene;
+
+
+    public override FluentNode Create()
+    {
+        return
+            Show() *
+            Write(0.25f, "Chamar inimigos para o combate?").WaitForButton() *
+            Options
+            (
+                Option("Sim") *
+                Hide() *
+                Do(() => ChangeScene()) *
+                End() *
+
+                Option("Não") *
+                Hide() *
+                End()
+            );
+    }
+
+    public override void OnFinish()
+    {
+        onFinish.Invoke();
+    }
+
+    public override void OnStart()
+    {
+        character.StopWalking();
+        onStart.Invoke();
+    }
+
+    public void ChangeScene()
+    {
+        changeScene.Invoke();
+    }
+}
