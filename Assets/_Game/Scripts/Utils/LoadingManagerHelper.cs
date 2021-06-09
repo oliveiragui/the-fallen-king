aa28dd7eb41d07a4821f28d15945979257822a9d
Samuel Souza Cabral <samuel.s.cabral@hotmail.com>
2021-06-09 00:21:19 -0300

    Alteração nas transições entre cenas

---
+++ Assets/_Game/Scripts/Utils/LoadingManagerHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Michsky.LSS;
+using UnityEngine;
+
+namespace _Game.Scripts.Utils
+{
+    public class LoadingManagerHelper : MonoBehaviour
+    {
+        [SerializeField] LoadingScreenManager loadingScreenManager;
+
+        [SerializeField] SceneData[] sceneData;
+
+        Dictionary<string, SceneData> indexedSceneData;
+
+        void Start()
+        {
+            indexedSceneData = new Dictionary<string, SceneData>();
+            foreach (var data in sceneData) indexedSceneData.Add(data.name, data);
+        }
+
+        public void Load(string sceneName)
+        {
+            var scene = indexedSceneData[sceneName];
+            loadingScreenManager.SetStyle(scene.style);
+            loadingScreenManager.LoadScene(scene.name);
+        }
+    }
+
+    [Serializable]
+    public class SceneData
+    {
+        public string name;
+        public string style;
+    }
+}
