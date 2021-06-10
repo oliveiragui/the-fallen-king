a9a13f4c9e32c860127826a3f114ec3df8985808
Samuel Souza Cabral <samuel.s.cabral@hotmail.com>
2021-06-10 01:54:17 -0300

    Adicionado placar ao morrer/finalizar modo onda

---
+++ Assets/_Game/Scripts/Utils/WaveSystem/ScoreCounter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using _Game.Scripts.Utils.MyBox.Attributes;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace _Game.Scripts.Utils
+{
+    public class ScoreCounter : MonoBehaviour
+    {
+        [SerializeField] int scoreValue;
+        [SerializeField] bool discountPerTime;
+        [ConditionalField("discountPerTime")] [SerializeField] int discountRate;
+        public PointScoredEvent pointScored;
+
+        int _currentScore;
+
+        Coroutine cr;
+
+        IEnumerator Discount()
+        {
+            _currentScore = scoreValue;
+            if (!discountPerTime) yield break;
+            while (enabled)
+            {
+                yield return new WaitForSeconds(1);
+                _currentScore -= discountRate;
+                if (_currentScore < 0) _currentScore = 0;
+            }
+        }
+
+        public void StartCount()
+        {
+            _currentScore = scoreValue;
+            if (cr != null) StopCoroutine(cr);
+            cr = StartCoroutine(Discount());
+        }
+
+        public void Score()
+        {
+            if (cr != null) StopCoroutine(cr);
+            pointScored.Invoke(_currentScore);
+        }
+    }
+
+    [Serializable]
+    public class PointScoredEvent : UnityEvent<int> { }
+}
